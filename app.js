e3cc0e791f6ccb70aa7081e415024ea3731d2004
Vivek ghoshi <the.vivekghoshi@gmail.com>
2025-05-22 13:28:20 +0530

    corrected

--- app.js
+++ app.js
@@ -16,10 +16,14 @@ const paymentRoutes = require('./routes/payment-routes');
 app.use(express.json()); 
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin: 'https://sheriverse-server.onrender.com', 
+    origin: 'https://sheriverse.onrender.com/', 
     credentials: true ,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.options('*', cors({
+  origin: 'https://sheriverse.onrender.com',
+  credentials: true,
 }));
 app.use(cookieParser()); 
 app.use('/api',commanRoutes);
